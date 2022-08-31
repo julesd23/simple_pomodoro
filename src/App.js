2dd28bde987d92e5fb73f0095ce2e5905531ce33
Julius DeAngelis <juliusdeangelis@gmail.com>
2022-08-31 16:02:54 -0400

    add settings and graphics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import Pomodoro from './Pomodoro';
+// import Pomodoro from './Pomodoro';
+import Timer from './Timer';
+import Settings from './Settings'
+import { useState } from 'react';
+import SettingsContext from './SettingsContext';
 
 function App() {
+
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(25);
+  const [breakMinutes, setBreakMinutes] = useState(5);
+
   return (
-    <div className="App">
-      <Pomodoro />
-    </div>
+    <main>
+      <SettingsContext.Provider value={{
+        showSettings,
+        setShowSettings,
+        workMinutes,
+        breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes,
+      }} >
+      {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
+    </main>
   );
 }
 
